3454549d7800911ca1e615be019e7610cd8003e5
Parth Tiwari <parthtiwari@Parths-MacBook-Air.local>
2024-07-05 02:26:33 +0530

    remove bcrypt from edge server

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -1,6 +1,6 @@
 import "server-only";
 import { redirect } from "next/navigation";
-import { auth } from "@/lib/auth";
+import { auth } from "@/lib/auth-no-edge";
 
 export async function checkAuth() {
   const session = await auth();
